44e2e14cfa33070545f7a54df0f46f65b9f1b8cd
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 06:27:05 +1000

    Implement new Parser.__place() to read in only valid placement instructions.

--- robot/parser.py
+++ robot/parser.py
@@ -4,6 +4,12 @@ from robot import Robot, Aim
 class Parser:
     def __init__(self, robot: Robot):
         self.__robot = robot
+        self.__map = {
+            'NORTH': Aim.NORTH,
+            'SOUTH': Aim.SOUTH,
+            'EAST': Aim.EAST,
+            'WEST': Aim.WEST
+        }
 
     def read(self, line: str):
         if line == 'MOVE':
@@ -14,5 +20,15 @@ class Parser:
             self.__robot.turn_right()
         elif line == 'REPORT':
             return self.__robot.report()
-        elif line == 'PLACE 3,4,NORTH':
-            return self.__robot.place(3, 4, Aim.NORTH)
+        elif line[:6] == 'PLACE ':
+            self.__place(line)
+
+    def __place(self, line: str):
+        parameters = line[6:].split(',')
+        if len(parameters) != 3:
+            return
+        if parameters[0].isdigit() and parameters[1].isdigit() and parameters[2] in self.__map:
+            x = int(parameters[0])
+            y = int(parameters[1])
+            facing = self.__map[parameters[2]]
+            self.__robot.place(x, y, facing)
