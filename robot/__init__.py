781951d4892d3ecb52f9c721a40990728462d2f1
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 05:11:22 +1000

    Create minimum Parser.read() implementation.

--- robot/__init__.py
+++ robot/__init__.py
@@ -1,4 +1,4 @@
 from .aim import Aim
 from .board import Board
-from .parser import Parser
 from .robot import Robot
+from .parser import Parser
--- robot/parser.py
+++ robot/parser.py
@@ -1,2 +1,9 @@
+from robot import Robot
+
+
 class Parser:
-    pass
+    def __init__(self, robot: Robot):
+        self.__robot = robot
+
+    def read(self, line: str):
+        self.__robot.move()
