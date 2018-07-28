ace1c6a109695e4f67ec555977d42e8e595846dc
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 08:21:54 +1000

    Create a mocked out input and output FileToFile test.

--- tests/unit/test_file_to_file_runner.py
+++ tests/unit/test_file_to_file_runner.py
@@ -1,7 +1,33 @@
+import io
+from robot import Parser
 from runner import FileToFileRunner
 from unittest import TestCase
+from unittest.mock import Mock, _Call as Call
 
 
 class TestFileToFileRunner(TestCase):
-    def test_creation(self):
-        x = FileToFileRunner()
+
+    def test_file_passed_to_parser(self):
+        """
+        FileToFile takes lines from input file, feeds to parser, and returns any results to the output.
+        """
+        contents = io.StringIO(initial_value="REPORT\nPLACE 1,1,NORTH \nMOVE\nREPORT", newline="\n")
+        expected_calls = [
+            Call(("read", ("REPORT",)), ),
+            Call(("read", ("PLACE 1,1,NORTH",)), ),
+            Call(("read", ("MOVE",)), ),
+            Call(("read", ("REPORT",)), ),
+        ]
+        expected_response = "1,2,NORTH"
+
+        output = io.StringIO()
+
+        parser = Mock(Parser)
+        parser.read.side_effect = [None, None, None, expected_response]
+
+        main = FileToFileRunner(contents, output, parser)
+        main.run()
+        output.seek(0)
+
+        parser.assert_has_calls(expected_calls)
+        self.assertEqual(expected_response, output.readline())
