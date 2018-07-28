718b589b3fb226cc9fa9a41fc9fec5a7a0ac2094
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 05:12:46 +1000

    Creating parse RIGHT test.

--- tests/unit/test_parser.py
+++ tests/unit/test_parser.py
@@ -18,3 +18,14 @@ class TestParser(TestCase):
         parser.read("MOVE")
 
         robot.move.assert_called_once()
+
+    def test_parse_left(self):
+        """
+        Reading LEFT calls Robot.turn_left()
+        """
+        robot = Mock(Robot)
+
+        parser = Parser(robot)
+        parser.read("LEFT")
+
+        robot.turn_left.assert_called_once()
