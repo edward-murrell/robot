ee63a7787b04202f720b033a541e07dcff7183b0
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-28 14:11:49 +1000

    Create red tests for immutable board properties.

--- tests/unit/test_board.py
+++ tests/unit/test_board.py
@@ -14,3 +14,21 @@ class TestBoard(TestCase):
         board = Board(height=45, width=16)
         self.assertEqual(45, board.height)
         self.assertEqual(16, board.width)
+
+    def test_immutable_height(self):
+        """
+        Board height properties are immutable.
+        """
+        board = Board(width=1, height=22)
+        with self.assertRaises(ValueError) as exc:
+            board.height = 8
+            self.assertEqual("Board height cannot be changed after creation.", str(exc))
+
+    def test_immutable_width(self):
+        """
+        Board width properties are immutable.
+        """
+        board = Board(width=4, height=4)
+        with self.assertRaises(ValueError) as exc:
+            board.width = 0
+            self.assertEqual("Board width cannot be changed after creation.", str(exc))
