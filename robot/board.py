49d5ca9d549838be21c7ba99f46dc2aba92dc68a
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-28 13:59:20 +1000

    Create minimum Board to satisfy unit tests.

--- robot/board.py
+++ robot/board.py
@@ -1,3 +1,10 @@
 class Board:
-    def __init__(self):
-        pass
+    """
+    Immutable board class.
+
+    Interrogate for height and width by attribute. These properties cannot be changed once created.
+    """
+
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
