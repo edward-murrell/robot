d08621e2e4bb3d231978a633c251549d587c3135
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-28 17:23:45 +1000

    Refactor location checking into it's own method.

--- robot/robot.py
+++ robot/robot.py
@@ -10,14 +10,8 @@ class Robot:
         self.__placed = False
 
     def place(self, x: int, y: int, direction: Aim):
-        if x >= self.__board.width:
-            return
-        if y >= self.__board.height:
-            return
-        if x < 0:
-            return
-        if y < 0:
-            return
+        if not self.__check_location(x, y):
+            return None
 
         self.__locX = x
         self.__locY = y
@@ -31,13 +25,13 @@ class Robot:
         self.__face = Aim((self.__face + 1) % 4)
 
     def move(self):
-        if self.__face == Aim.NORTH and self.__locY + 1 < self.__board.height:
+        if self.__face == Aim.NORTH and self.__check_location(self.__locX, self.__locY + 1):
             self.__locY += 1
-        elif self.__face == Aim.SOUTH and self.__locY - 1 >= 0:
+        elif self.__face == Aim.SOUTH and self.__check_location(self.__locX, self.__locY - 1):
             self.__locY -= 1
-        if self.__face == Aim.EAST and self.__locX + 1 < self.__board.width:
+        elif self.__face == Aim.EAST and self.__check_location(self.__locX + 1, self.__locY):
             self.__locX += 1
-        elif self.__face == Aim.WEST and (self.__locX - 1) >= 0:
+        elif self.__face == Aim.WEST and self.__check_location(self.__locX - 1, self.__locY):
             self.__locX -= 1
 
     def report(self):
@@ -45,3 +39,14 @@ class Robot:
             return f"{self.__locX},{self.__locY},{self.__face.name}"
         else:
             return None
+
+    def __check_location(self, x: int, y: int) -> bool:
+        if x >= self.__board.width:
+            return False
+        if y >= self.__board.height:
+            return False
+        if x < 0:
+            return False
+        if y < 0:
+            return False
+        return True
