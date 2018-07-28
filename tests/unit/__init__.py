2ec8b363763f99d07420a046ecc83168a3f08db2
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 06:53:52 +1000

    Create first test for File to File runner.

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,3 +1,4 @@
 from .test_board import TestBoard
 from .test_robot import TestRobot
 from .test_parser import TestParser
+from .test_file_to_file_runner import TestFileToFileRunner
---
+++ tests/unit/test_file_to_file_runner.py
@@ -0,0 +1,7 @@
+from runner import FileToFileRunner
+from unittest import TestCase
+
+
+class TestFileToFileRunner(TestCase):
+    def test_creation(self):
+        x = FileToFileRunner()
