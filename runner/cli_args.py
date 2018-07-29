807f37fc0525997da106dd835378f25ad8df41ee
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 14:11:37 +1000

    Simplify and document CliArgs.

--- runner/cli_args.py
+++ runner/cli_args.py
@@ -2,15 +2,19 @@ from argparse import ArgumentParser, FileType
 
 
 class CliArgs:
+    """
+    Parse and open input files for use in robot.py
+    """
 
     @staticmethod
-    def parse(arguments):
-        parser = CliArgs.__build()
+    def parse(arguments: list):
+        """
+        Parse a list of arguments.
+
+        :param arguments: List key/value pairs arguments, such as from sys.argv.
+        :return: Namespace, with open file in file key.
+        """
+        parser = ArgumentParser()
+        parser.add_argument('--file', dest="file", type=FileType('r'))
         config = parser.parse_args(arguments)
         return config
-
-    @staticmethod
-    def __build() -> ArgumentParser:
-        argparser = ArgumentParser()
-        argparser.add_argument('--file', dest="file", type=FileType('r'))
-        return argparser
