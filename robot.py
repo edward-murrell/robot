ae60e9adfb88c9d9efe514a71f7a1fe8d3433343
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 20:51:55 +1000

    Add missing main program initiator documentation.

--- robot.py
+++ robot.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python
+"""
+Robot Program.
+
+Copyright 2018, Edward Murrell
+"""
 import sys
 from runner import CliArgs, FileToFileRunner
 from robot import Robot, Board, Parser
 
 
 def main(arguments):
+    """
+    Run the program using file as input.
+
+    Out is written standard out.
+
+    Argument list should contain ['--file', <filename>]
+
+    :param arguments: Arguments from sys.argv, with script name removed.
+    """
     config = CliArgs.parse(arguments)
     runner = FileToFileRunner(config.file, sys.stdout, Parser(Robot(Board(5, 5))))
     runner.run()
