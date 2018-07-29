e061971afedff9a3d1bf6441aae25d727e7542ee
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 14:13:02 +1000

    Create robot.py to pass existing functional test.

--- tests/functional/test_output.py
+++ tests/functional/test_output.py
@@ -6,7 +6,7 @@ class TestOutput(TestCase):
     def test_example_one(self):
         expected = "0,1,NORTH"
 
-        completed = subprocess.run(["python3.5", "robot.py", "--file", "tests/provided/example1.txt"])
+        completed = subprocess.run(["python3.6", "robot.py", "--file", "tests/provided/example1.txt"])
 
         self.assertEqual(0, completed.returncode)
 
