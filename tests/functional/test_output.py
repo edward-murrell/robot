ba45e4bbb5500215bbae5ad520051fc3a8ee48e4
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 14:31:37 +1000

    Add the second and third examples of programs given by the requirements documentation as functional tests.

--- tests/functional/test_output.py
+++ tests/functional/test_output.py
@@ -4,6 +4,9 @@ from unittest import TestCase
 
 class TestOutput(TestCase):
     def test_example_one(self):
+        """
+        Test the first example given by the requirements document
+        """
         expected = b"0,1,NORTH\n"
 
         completed = subprocess.run(["python3.6", "robot.py", "--file", "tests/provided/example1.txt"],
@@ -12,3 +15,23 @@ class TestOutput(TestCase):
         self.assertEqual(0, completed.returncode)
         self.assertEqual(expected, completed.stdout)
 
+    def test_example_two(self):
+        expected = b"0,0,WEST\n"
+
+        completed = subprocess.run(["python3.6", "robot.py", "--file", "tests/provided/example2.txt"],
+                                   stdout=subprocess.PIPE)
+
+        self.assertEqual(0, completed.returncode)
+        self.assertEqual(expected, completed.stdout)
+
+    def test_example_three(self):
+        """
+        Test the first example given by the requirements document
+        """
+        expected = b"3,3,NORTH\n"
+
+        completed = subprocess.run(["python3.6", "robot.py", "--file", "tests/provided/example3.txt"],
+                                   stdout=subprocess.PIPE)
+
+        self.assertEqual(0, completed.returncode)
+        self.assertEqual(expected, completed.stdout)
