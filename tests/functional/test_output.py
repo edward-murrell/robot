59b04795384629c2eda8c4e7beb7908cb5088b71
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 14:18:29 +1000

    Assertion for output to get complete output from the top level program.

--- tests/functional/test_output.py
+++ tests/functional/test_output.py
@@ -6,7 +6,9 @@ class TestOutput(TestCase):
     def test_example_one(self):
         expected = "0,1,NORTH"
 
-        completed = subprocess.run(["python3.6", "robot.py", "--file", "tests/provided/example1.txt"])
+        completed = subprocess.run(["python3.6", "robot.py", "--file", "tests/provided/example1.txt"],
+                                   stdout=subprocess.PIPE)
 
         self.assertEqual(0, completed.returncode)
+        self.assertEqual(expected, completed.stdout)
 
