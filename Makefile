ba758bc89a3928277db1de47fd86cad44c3440b7
Edward Murrell <edward.murrell@previousnext.com.au>
2018-07-29 14:12:12 +1000

    Create framework for functional tests.

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-.PHONY: tests unit_tests
+.PHONY: tests unit_tests functional_tests
 
-tests: unit_tests
+tests: unit_tests functional_tests
 
 unit_tests:
 	python3.6 -m unittest tests.unit
+
+functional_tests:
+	python3.6 -m unittest tests.functional
---
+++ tests/functional/test_output.py
@@ -0,0 +1,12 @@
+import subprocess
+from unittest import TestCase
+
+
+class TestOutput(TestCase):
+    def test_example_one(self):
+        expected = "0,1,NORTH"
+
+        completed = subprocess.run(["python3.5", "robot.py", "--file", "tests/provided/example1.txt"])
+
+        self.assertEqual(0, completed.returncode)
+
